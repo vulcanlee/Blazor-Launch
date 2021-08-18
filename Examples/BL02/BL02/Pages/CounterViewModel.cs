c692bd84599352034da979eb1712f3ac51c87b58
Vulcan <vulcan.lee@gmail.com>
2021-08-18 17:48:55 +0800

    建立一個新的元件(頁面)

---
+++ Examples/BL02/BL02/Pages/CounterViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BL02.Pages
+{
+    public class CounterViewModel
+    {
+        public int currentCount { get; set; }
+
+        public void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
