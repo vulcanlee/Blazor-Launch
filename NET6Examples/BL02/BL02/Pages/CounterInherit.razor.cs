20aab59e59ab03b894d74d097346fe9dba0c3d94
Vulcan <vulcan.lee@gmail.com>
2021-10-18 11:49:30 +0800

    Blazor 元件程式碼設計五種選擇方式

---
+++ NET6Examples/BL02/BL02/Pages/CounterInherit.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BL02.Pages
+{
+    public class CounterInheritBase : ComponentBase
+    {
+        public int currentCount = 0;
+
+        public void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
---
+++ NET6Examples/BL02/BL02/Pages/CounterPartialClass.cs
@@ -0,0 +1,12 @@
+﻿namespace BL02.Pages
+{
+    public partial class CounterPartialClass
+    {
+        private int currentCount = 0;
+
+        void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
